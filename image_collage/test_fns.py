d4f3fbf830ba401a7492299f23296eb605360df5
zrh_datalab <11912614@mail.sustech.edu.cn>
2024-04-17 21:29:15 -0500

    finish painting mask on the image

--- image_collage/test_fns.py
+++ image_collage/test_fns.py
@@ -1,40 +1,56 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from remove_anything.remove_anything_function import remove_anything, test_mask
+
+import time
+
 
 
 class TestImageProcesser:
     def __init__(self) -> None:
-        self.pos = None
         self.mask = None
-        self.img_masked = None
+        self.original = None
+        
+        self.sam_model_type = "vit_h"
+        self.sam_ckpt = "./remove_anything/pretrained_models/sam_vit_h_4b8939.pth"
+        self.lama_config = "./remove_anything/lama/configs/prediction/default.yaml"
+        self.lama_ckpt = "./remove_anything/pretrained_models/big-lama"
+        self.dilate_kernel_size = 15
+        
     
     def click_process(self, img: np.ndarray, coords: list):
         """
         img: RGB, (H, W, C)
         coords: (H, W)
         """
+        print(img.shape)
+        # cv2.imwrite("test.png", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
         
         if self.mask is not None:
-            img[self.pos[0]: self.pos[1], self.pos[2]: self.pos[3]] = self.img_masked
-            self.mask = self.pos = self.img_masked = None
+            img = self.original
+            self.mask = self.original = None
         
-        radius = 10
-        color = (255, 0, 0)
-        line_type = -1
-        x, y = coords
         
-        self.pos = (x - radius, x + radius, y - radius, y + radius)
-        self.mask = np.zeros(img.shape[:2]).astype(bool)
-        self.mask[self.pos[0]: self.pos[1], self.pos[2]: self.pos[3]] = True
-        self.img_masked = img[self.pos[0]: self.pos[1], self.pos[2]: self.pos[3]].copy()
+        masks = test_mask(img, coords, [1], self.sam_model_type, self.sam_ckpt)
+        mask = masks[1]
+        self.mask = mask == 255
         
-        cv2.circle(img, (y, x), radius, color, line_type)
+        # store original img
+        self.original = img.copy()
+        
+        # paint mask
+        colored_mask = np.zeros_like(img)
+        colored_mask[self.mask] = (255, 0, 0) # red
+        img = cv2.addWeighted(img, 1, colored_mask, 0.65, 0)
         
         return img
     
     def replace(self, img, text):
 
         img[self.mask] = np.array([255,255,255])
+        
+        # remove_anything(img, dilate_kernel_size, lama_config, lama_ckpt, masks)
         
         self.mask = None
         self.img_masked = None
